8326804bcc299210f2d553fae953920f2257de9f
Siba-Ka-Playground <bariksiba33@gmail.com>
2026-01-20 21:29:01 +0530

    Updated the Code

--- setup.py
+++ setup.py
@@ -2,19 +2,18 @@ from setuptools import setup, find_packages
 
 setup(
     name='shadowscan',
-    version='1.1',
+    version='1.2',
     description='Offensive OSINT Framework',
     author='Sibasundar Barik',
+    # This automatically finds the 'shadowscan_core' folder
     packages=find_packages(),
-    # --- THE FIX IS HERE ---
-    py_modules=['main'], 
-    # -----------------------
     install_requires=[
         'requests', 'rich', 'pyfiglet', 'easyocr', 
         'spacy', 'exifread', 'textblob', 'thefuzz', 'pillow-heif'
     ],
+    # The magic link: Package -> File -> Function
     entry_points='''
         [console_scripts]
-        shadowscan=main:main
+        shadowscan=shadowscan_core.main:main
     ''',
 )
