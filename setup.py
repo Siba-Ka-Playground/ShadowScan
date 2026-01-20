0cc7cecdea0da9eb02602251d8586ccb242e1f61
Siba-Ka-Playground <bariksiba33@gmail.com>
2026-01-20 20:43:39 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,9 +3,10 @@ from setuptools import setup, find_packages
 setup(
     name='shadowscan',
     version='1.1',
-    description='Offensive OSINT Framework for Recursive Scanning',
+    description='Offensive OSINT Framework',
     author='Sibasundar Barik',
-    packages=find_packages(),
+    packages=find_packages(),      
+    py_modules=['main'],           
     install_requires=[
         'requests', 'rich', 'pyfiglet', 'easyocr', 
         'spacy', 'exifread', 'textblob', 'thefuzz', 'pillow-heif'
